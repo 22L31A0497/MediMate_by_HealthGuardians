025455b8ec76c74adec81e07cd5cd1cf87bd52d3
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-04-13 05:54:04 +0530

    final version

--- src/components/VedioCall.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function VedioCall() {
-  return (
-    <div>
-      <h1 className='p-14 text-5xl align-middle justify-center bg-red-700'>this is Video call page between patient and doctor</h1>
-      </div>
-  )
-}
